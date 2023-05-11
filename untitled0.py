00ab1034937291fb1333c3326c9e217cf20226be
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2023-05-11 20:44:11 +0300

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 15:42:44 2023
+
+@author: daphn
+"""
+
+import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+import pyautogui as auto
+
+cap=cv2.VideoCapture(0)
+hd=HandDetector(maxHands=1)
+
+while 1:
+    rt,frame=cap.read()
+    frame=cv2.resize(frame,(900,720))
+    cvzone.putTextRect(frame,'DINO GAME HACK',(360,40),scale=3,thickness=3,border=2)
+    hand,frame=hd.findHands(frame)
+    if hand:
+        hands=hand[0]
+        lmlist=hands['lmList']
+        #print(lmlist)
+        length,info,frame=hd.findDistance(lmlist[4][0:2],lmlist[8][0:2],frame)
+        length=round(length)
+        
+        if length<25:
+            auto.press('up')
+            
+    cv2.imshow('frame',frame)
+    cv2.waitKey(1)
